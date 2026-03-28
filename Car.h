52e9873a8f58be228d6647a2e9a780bcaee487ad
dan-pershounin <119265995+dan-pershounin@users.noreply.github.com>
2026-03-27 20:55:48 -0400

    feat: add car struct in Car.h

---
+++ Car.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+
+
+struct Car {
+    int id;
+    std::string brand;
+    std::string model;
+    int year;
+    std::string color;
+    int mileage;
+    int price;
+    std::string condition;
+};
+
+
+#define REDBLACKBCARS_CAR_H
+
+// Created by danpe on 3/22/2026.
